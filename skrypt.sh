1f49c210aed9265362dd0f964aebbb96a2a723e3
adakde <kuba-kasprzak12@wp.pl>
2023-06-28 18:24:02 +0200

    Dodaj implementację nowych funkcji

--- skrypt.sh
+++ skrypt.sh
@@ -1,29 +1,78 @@
-
 #!/bin/bash
 
-if [[ "$1" == "--date" ]]; then
-    date
-elif [[ "$1" == "--logs" ]]; then
-    if [[ -z "$2" ]]; then
-        echo "Podaj liczbę plików jako drugi argument."
-    else
-        for ((i=1; i<=$2; i++)); do
-            echo "Nazwa pliku: log$i.txt"
-            echo "Nazwa skryptu: skrypt.sh"
-            echo "Data: $(date)"
-            echo
-            echo "Zawartość pliku: " >> "log$i.txt"
-            echo "Nazwa pliku: log$i.txt" >> "log$i.txt"
-            echo "Nazwa skryptu: skrypt.sh" >> "log$i.txt"
-            echo "Data: $(date)" >> "log$i.txt"
-        done
-        echo "Utworzono $2 plików log."
-    fi
-elif [[ "$1" == "--help" ]]; then
-    echo "Dostępne opcje:"
-    echo "--date : Wyświetla dzisiejszą datę."
-    echo "--logs [liczba] : Tworzy określoną liczbę plików log i zapisuje w nich informacje."
-    echo "--help : Wyświetla wszystkie dostępne opcje."
-else
-    echo "Nieznana opcja. Użyj --help, aby zobaczyć dostępne opcje."
-fi
+# Funkcja wyświetlająca pomoc
+display_help() {
+  echo "Skrypt.sh - Skrypt bash"
+  echo "Dostępne opcje:"
+  echo "  --help, -h          Wyświetla pomoc"
+  echo "  --logs, -l          Tworzy 100 plików logx/logx.txt"
+  echo "  --logs N            Tworzy N plików logx/logx.txt"
+  echo "  --date, -d          Wyświetla dzisiejszą datę"
+  echo "  --init              Klonuje repozytorium i ustawia ścieżkę w PATH"
+  echo "  --error, -e N       Tworzy N plików errorx/errorx.txt"
+}
+
+# Funkcja tworząca pliki log
+create_log_files() {
+  local count=$1
+  for ((i=1; i<=count; i++))
+  do
+    echo "Nazwa pliku: log$i.txt" > log$i/log$i.txt
+    echo "Nazwa skryptu: skrypt.sh" >> log$i/log$i.txt
+    echo "Data: $(date)" >> log$i/log$i.txt
+  done
+}
+
+# Funkcja tworząca pliki error
+create_error_files() {
+  local count=$1
+  for ((i=1; i<=count; i++))
+  do
+    echo "Nazwa pliku: error$i.txt" > error$i/error$i.txt
+    echo "Nazwa skryptu: skrypt.sh" >> error$i/error$i.txt
+    echo "Data: $(date)" >> error$i/error$i.txt
+  done
+}
+
+# Obsługa flag
+while [[ $# -gt 0 ]]
+do
+  key="$1"
+
+  case $key in
+    --help|-h)
+      display_help
+      exit 0
+      ;;
+    --logs|-l)
+      create_log_files 100
+      exit 0
+      ;;
+    --logs)
+      create_log_files "$2"
+      exit 0
+      ;;
+    --date|-d)
+      echo $(date)
+      exit 0
+      ;;
+    --init)
+      git clone https://github.com/adakde/lab04.git
+      export PATH=$PATH:$(pwd)/lab04
+      exit 0
+      ;;
+    --error|-e)
+      create_error_files 100
+      exit 0
+      ;;
+    --error|-e)
+      create_error_files "$2"
+      exit 0
+      ;;
+    *)
+      echo "Nieznana opcja: $key"
+      exit 1
+      ;;
+  esac
+  shift
+done
